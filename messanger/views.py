9cc9207489749823faf7881769c8cd7ae0b9e6b2
hamidreza <hamidranjbarpour771@gmail.com>
2020-09-19 13:18:47 +0430

    chat room template created and added to urlconf

--- messanger/views.py
+++ messanger/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'messanger/index.html')
+
+
+def room(request, room_name):
+    return render(request, 'messanger/room.html', {
+        'room_name': room_name
+    })
