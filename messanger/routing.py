38ef0166b8a0c1d466a759cb5c2bcf955c56ba53
hamidreza <hamidranjbarpour771@gmail.com>
2020-10-01 00:54:25 +0330

    path replaced with re_path due to url router limitations.

--- messanger/routing.py
+++ messanger/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumers.ChatConsumer)
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
