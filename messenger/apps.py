7c3e583f01a7ea7069c3ab35c612cec042daa640
hamidreza <hamidranjbarpour771@gmail.com>
2020-10-11 21:14:35 +0330

    app rename required changes applied

--- messenger/apps.py
+++ messenger/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class MessangerConfig(AppConfig):
-    name = 'messanger'
+    name = 'messenger'
--- messenger/views.py
+++ messenger/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'messanger/index.html')
+    return render(request, 'messenger/index.html')
 
 
 def room(request, room_name):
-    return render(request, 'messanger/room.html', {
+    return render(request, 'messenger/room.html', {
         'room_name': room_name
     })
