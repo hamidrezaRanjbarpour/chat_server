0f4519da1a560fb740d4dd7d9da4c7a67c983b97
hamidreza <hamidranjbarpour771@gmail.com>
2020-10-25 23:13:28 +0330

    database setup to load and store messages.

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=150)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    # get 10 most recent messages
+    def get_most_recent_messages(self):
+        return self.objects.all().order_by('-time_stamp')[:10]
+
+
